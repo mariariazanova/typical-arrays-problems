727b3923ea9413ddfa0083432eed74d839a6f8b8
Maria Riazanova <mv_13@mail.ru>
2020-09-29 23:35:17 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  let result = array[0]; 
+  for(let i = 0; i < array.length; i++){
+      if (result > array[i]) result = array[i]
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  let result = array[0]; 
+  for(let i = 0; i < array.length; i++){
+      if (result < array[i]) result = array[i]
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  return array.reduce((sum, i) => sum + i,0)/array.length;
 }
